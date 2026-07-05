e0455eb04fba436502de6fcd3cd69c6a07f02fa7
emrekctp89 <emrekocatepe89@gmail.com>
2026-07-05 15:07:54 +0300

    fix(config): Revert next.config.js to original (remove webpack config)

--- next.config.js
+++ next.config.js
@@ -88,13 +88,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /supabase\/functions/,
-      loader: 'ignore-loader',
-    });
-    return config;
-  },
 };
 
 module.exports = withBundleAnalyzer(
