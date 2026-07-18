ea23cbc5828decd74441a299209a3f0760cfe796
emrekctp89 <213290509+emrekctp89@users.noreply.github.com>
2026-07-18 21:39:01 +0300

    feat: add Google Tag Manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,6 +34,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="tr" suppressHydrationWarning>
       <head>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T6J56FT3');`}
+        </Script>
         <Script
           id="consentmanager-cmp"
           strategy="beforeInteractive"
@@ -69,7 +76,18 @@ export default function RootLayout({ children }) {
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
         <link rel="alternate" type="application/rss+xml" href="/rss.xml" />
       </head>
-      <body className={`${onest.className} bg-background text-foreground`}>{children}</body>
+      <body className={`${onest.className} bg-background text-foreground`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T6J56FT3"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
